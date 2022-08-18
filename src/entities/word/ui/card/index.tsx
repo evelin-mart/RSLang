157e29a7fb3ee32dea06bc31ca37b6b8c2bd4d1f
evelin-mart <evalouis666@gmail.com>
2022-08-18 23:33:52 +0300

    init: start task

--- src/entities/word/ui/card/index.tsx
+++ src/entities/word/ui/card/index.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Word } from 'entities/word/model';
-import { Paper, Typography, Card } from '@mui/material';
+import { Paper, Typography, Card, CardMedia } from '@mui/material';
+import styles from './styles.module.scss'
 
 export type WordCardProps = {
   word: Word;
 }
 
+// const baseUrl = 'https://react-learnwords-jsfe2022.herokuapp.com/';
+const baseUrl = 'http://localhost:3001/';
+
 export const WordCard = (props: WordCardProps) => {
   const { word } = props;
 
   return (
-    <Card sx={{ padding: 1 }}>
-      <Typography variant="subtitle1" component="p">
+    <Card sx={{ display: 'flex' }} className={styles.container}>
+      <CardMedia component='img' sx={{ flexBasis: '30%' }} image={`${baseUrl}${word.image}`} alt={word.word} />
+      <Typography variant='subtitle1' component='p'>
         {word.word}
       </Typography>
-      <Typography variant="subtitle1" component="p">
+      <Typography variant='subtitle1' component='p'>
         {word.wordTranslate}
       </Typography>
     </Card>
-  )
+  );
 }
