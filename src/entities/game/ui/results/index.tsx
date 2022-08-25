10c25e15503882f932026fc5452198890102d712
Alexander Tretyakov <tpetyakov@gmail.com>
2022-08-25 18:40:36 +0300

    feat: improve game results output

--- src/entities/game/ui/results/index.tsx
+++ src/entities/game/ui/results/index.tsx
@@ -1,47 +1,66 @@
-import { Link } from 'react-router-dom';
-import { games } from 'shared/constants/games';
-import { setGameId, useGame, useGameResults, resetGame, setGameSource } from 'entities/game';
+import { useGameResults, replayGame, resetGame } from 'entities/game';
 import { AppDispatch } from 'app/store';
 import { useDispatch } from 'react-redux';
-import { Box, Typography, Button } from '@mui/material';
+import { Box, Typography, Button, Divider } from '@mui/material';
 import { ResultsWordsList } from './words-list';
 import { GameInformationWrapper } from '../info-wrapper';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import styles from './styles';
+
+export type Sound = HTMLAudioElement | null;
 
 export const GameResults = () => {
   const dispatch: AppDispatch = useDispatch();
-  const { gameId, source } = useGame();
+  const [sound, setSound] = React.useState<Sound>(null);
   const { correctWords, failedWords } = useGameResults();
-  if (!gameId) return <></>;
+  const navigate = useNavigate();
 
-  const { title } = games[gameId];
-  
   const handleReplay = () => {
-    dispatch(resetGame()); // maybe make separate action for replay?
-    dispatch(setGameId(gameId));
-    dispatch(setGameSource(source));
+    dispatch(replayGame());
   }
 
+  const handleToTextbookClick = () => {
+    dispatch(resetGame());
+    navigate('/textbook', { replace: true });
+  }
+
+  const correctWordsPercent = correctWords.length / (failedWords.length + correctWords.length);
+  const result = correctWordsPercent >= 0.6
+    ? 'Отличный результат!'
+    : 'Такой себе результат';
+
   return (
-    <GameInformationWrapper title={title}>      
-      <Box>
-        <Typography variant="h5">
-          Отличный результат: 
+    <GameInformationWrapper>      
+      <Typography variant="h5" sx={{ textAlign: "center", fontSize: { xs: "1.5rem", sm: "2rem"}, color: "grey.700" }}>
+        {result} 
+      </Typography>
+      <Box sx={styles.wordsList}>
+        <Typography variant="body2" sx={[{ color: "error.light" }, styles.answersRow]}>
+          <ErrorOutlineIcon fontSize="small" /> ошибок: {failedWords.length}
         </Typography>
-        <Typography variant="body2" sx={{ color: "error.light" }}>
-          ошибок: {failedWords.length}
+        <ResultsWordsList words={failedWords} sound={sound} setSound={setSound}/>
+        <Divider sx={{ width: "50%", mb: 1 }} />
+        <Typography variant="body2" sx={[{ color: "success.light" }, styles.answersRow]}>
+          <TaskAltIcon fontSize="small" /> верных ответов: {correctWords.length}
         </Typography>
-        <Typography variant="body2" sx={{ color: "success.light" }}>
-          верных ответов: {correctWords.length}
-        </Typography>
+        <ResultsWordsList words={correctWords} sound={sound} setSound={setSound}/>
       </Box>
-      <ResultsWordsList correctWords={correctWords} failedWords={failedWords} />
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
-        {source === 'textbook' 
-          && <Link to='/textbook'>Вернуться в учебник</Link>}
+      <Box sx={{ display: "flex", justifyContent: "space-between", mt: 1 }}>
+        <Button
+          onClick={handleToTextbookClick}
+          variant="contained"
+          color="secondary">
+          Учебник
+        </Button>
         <Button 
           onClick={handleReplay}
           variant="contained" 
-          color="secondary">Сыграть еще раз</Button>
+          color="secondary">
+          Сыграть еще раз
+        </Button>
       </Box>
     </GameInformationWrapper>
   )
--- src/entities/game/ui/results/words-list/index.tsx
+++ src/entities/game/ui/results/words-list/index.tsx
@@ -1,39 +1,61 @@
-import { Box, List, ListItem } from '@mui/material';
+import { List, ListItem, Typography, IconButton } from '@mui/material';
 import { AggregatedWord } from 'shared/api/users-aggregated-words';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import styles from '../styles';
+import React from 'react';
+import { makeAbsUrl } from 'shared/constants';
+import { playSong } from 'pages/textbook/utils';
+import { Sound, useGame } from 'entities/game';
+
 
 type WordsListProps = {
-  correctWords: AggregatedWord[];
-  failedWords: AggregatedWord[];
+  words: AggregatedWord[];
+  sound: Sound;
+  setSound: React.Dispatch<React.SetStateAction<Sound>>;
 }
 
-export const ResultsWordsList = ({ correctWords, failedWords }: WordsListProps) => {
-  
-  const correctListItems = correctWords.map((word) => {
+export const ResultsWordsList = ({ words, sound, setSound }: WordsListProps) => {
+  const { isSound } = useGame();
+
+  React.useEffect(() => {
+    if (sound) {
+      playSong(sound)
+        .finally(() => setSound(null));
+    }
+    return () => {
+      sound && sound.pause();
+    };
+  }, [sound, setSound]);
+
+  const handlePlay = (audioUrlPath: string) => {
+    if (isSound) {
+      setSound(new Audio(makeAbsUrl(audioUrlPath)));
+    }
+  }
+
+  const listItems = words.map((word) => {
     return (
-      <ListItem 
-        key={word.id} 
-        sx={{ color: "success.light" }}>
-        {word.word} - {word.wordTranslate}
+      <ListItem key={word.id} sx={{ p: 0, mb: 1 }} >
+        <IconButton
+          disabled={sound !== null || !isSound}
+          size="small"
+          onClick={() => handlePlay(word.audio)}
+          sx={styles.playButton}>
+          <VolumeUpIcon />
+        </IconButton>
+        <Typography component="span">
+          {word.word} 
+        </Typography>
+        <Typography component="span" sx={{ color: "grey.500"}}>
+          &nbsp;–&nbsp;{word.wordTranslate}
+        </Typography>
       </ListItem>
     );
   });
 
-  const failedListItems = failedWords.map((word) => {
-    return (
-      <ListItem 
-        key={word.id} 
-        sx={{ color: "error.light" }}>
-        {word.word} - {word.wordTranslate}
-      </ListItem>
-    );
-  });
-    
   return (
-    <Box sx={{ height: 200, overflowY: "auto", pt: 1, pb: 1, bgcolor: "grey.50" }}>
-      <List>
-        {failedListItems}
-        {correctListItems}          
-      </List>
-    </Box>
+    <List sx={{ p: 0 }}>
+      {listItems}
+    </List>
   )
 }
